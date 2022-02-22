32391462c77dfed7655e1f5abcb64ddbce347f22
John Page <john.page@10gen.com>
2022-02-22 10:58:01 +0000

    Update getreading.sh

--- getreading.sh
+++ getreading.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 libcamera-still -n -v 0 --hflip --vflip -o out.jpg
 
 convert out.jpg -crop 600x200+170+600 -negate -gaussian-blur 20 -threshold 70% -rotate 10 -shear -15x0 -resize 25% new.jpg
@@ -6,9 +8,10 @@ tesseract  new.jpg new --psm 7  -l ssd --dpi 70
 export READING=$(head -1 new.txt | sed 's/^0*//g' )
 echo $READING
 export TYPE="gas"
-(( $(echo "$READING > 26000" |bc -l) )) && export TYPE="electric"
+[ $(echo "$READING > 26000" |bc -l) -eq 1 ] && export TYPE="electric"
+echo $TYPE
 
-export APIKEY="XXXX"
+export APIKEY="XXXXXXXXXXXXXXXX-XXXXXXXX"
 export EPOCHMILLIS=$(date +%s000)
 
  curl --location --request POST 'https://data.mongodb-api.com/app/data-amzuu/endpoint/data/beta/action/insertOne' \
@@ -40,7 +43,8 @@ export READING=$(head -1 new2.txt | sed 's/^0*//g' )
 echo $READING
 
 export TYPE="gas"
-(( $(echo "$READING > 26000" |bc -l) )) && export TYPE="electric"
+[ $(echo "$READING > 26000" |bc -l) -eq 1 ] && export TYPE="electric"
+echo $TYPE
 
 export EPOCHMILLIS=$(date +%s000)
 
